fc288f98605b343d82a3cddec5f3f38767b52ebe
deepesh Nagarkoti <deepesh.nagarkoti@gmail.com>
2019-07-30 11:18:41 -0700

    Create signin page. And hookup signin action creator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,40 +1,38 @@
-const User = require("./../models/User");
-const jwt = require('jwt-simple');
-const config = require('./../config');
+const db      = require('./../models');
+const jwt     = require('jwt-simple');
+const config  = require('./../config');
 
-const tokenForUSer = function (user) {
-    const timestamp = new Date().getTime();
-    // Sub === subject
-    // iat === issued at time
+const tokenForUser = function(user) {
+  const timestamp = new Date().getTime();
+  // Sub === subject
+  // iat === issued at time
 
-    // Its going to encode the whole !st object and add our secret to it
-    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+  // Its going to encode the whole 1st object and then add our secret to it
+  return jwt.encode({ sub: user.id, iat: timestamp}, config.secret);
 };
 
 
 module.exports = {
-    signUp: async (req, res) => {
-        const { email, password } = req.body;
-
-        if (!email || !password) {
-            return res.status(422).json({ error: 'You must provide an email and password' });
-        }
-        try {
-            const existingUser = await User.findOne({ email });
-            if (existingUser) {
-                return res.status(422).json({ error: 'email is in use' });
-            }
-
-            const user = new User({ email, password });
-            await user.save();
-            res.json({ token: tokenForUSer(user) });
-        } catch (e) {
-            console.log(e)
-            res.status(404).json({ e });
-        }
-    },
-    signIn: (req, res) => {
-        res.send({token: tokenForUSer(req.user)});
+  signUp: async (req, res) => {
+    const { email, password } = req.body;
+    if(!email || !password) {
+      return res.status(422).json({ error: 'You must provide an email and password' });
     }
-}
-
+    try {
+      // Check if theres existing user
+      const existingUser = await db.User.findOne({ email });
+      // if user exist, throw error
+      if(existingUser) {
+        return res.status(422).json({ error: 'Email is in use' });
+      }
+      const user = new db.User({ email, password });
+      await user.save();
+      res.json({ token: tokenForUser(user)});
+    } catch(e) {
+      res.status(404).json({ e });
+    }
+  },
+  signIn: (req, res) => {
+    res.send({ token: tokenForUser(req.user)});
+  }
+};
